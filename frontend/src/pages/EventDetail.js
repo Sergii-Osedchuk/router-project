21e0b85869c6493293723b277db03a9bbb72d5d1
Sergio <norgestream@gmail.com>
2024-05-13 13:55:31 +0300

    add links to eventsnavigation

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,3 +1,13 @@
+import { useParams } from 'react-router-dom';
+
 export default function EventDetailPage() {
-  return <h1>EventDetailPage</h1>
+  const params = useParams();
+
+  return (
+    <>
+      <h1>EventDetailPage</h1>
+      <p>Event id: {params.eventId}</p>
+    </>
+    
+  )
 }
